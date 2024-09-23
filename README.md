15b3a72c166a5b6810040a463e3cd7bb6ab330f8
maybe-hello-world <maybe.hello.world@gmail.com>
2024-09-22 18:41:14 -0700

    implementation

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # ah2o
 Apple Health 2 Obsidian
+
+## Description
+
+This is a simple REST API to get Apple Health data and write it down to daily notes of Obsidian. I use [Health Auto Export](https://apps.apple.com/us/app/health-auto-export-json-csv/id1115567069) app to export data from Apple Health and push it to this API which then writes it down to daily notes in Obsidian. Obsidian vault is accessible on the server where this API is running (in my case - via OneDrive).
+
+I implemented this just because I want to store this data in Obsidian. Unfortunately, Health Auto Export app is paid (6$/year), but it's easier than doing it manually. If you know how to do it manually or can implement it and put to Open Source, please tell me and I'll be happy to use it.
+
+## Installation
+
+Run the Docker container:
+    
+```bash
+docker run -d -p 8000:8000 --name ah2o --restart=always -e AH2O_SECRET_KEY=<unique secret key> -e AH2O_DAILY_NOTES_LOCATION=/daily_notes -e AH2O_CONVERT_LB_TO_KG=true -v /path/to/vault/daily_notes:/daily_notes maybehelloworld/ah2o
+```
+
+AH2O_SECRET_KEY - unique secret key to authorize requests, just create a random string  
+AH2O_DAILY_NOTES_LOCATION - location of daily notes in Obsidian vault connected (on your filesystem)
+AH2O_CONVERT_LB_TO_KG - convert weight from lb to kg (true/false)
+
+
+Install Health Auto Export app and configure it to export data to `http://<your-ip>:8000/healthmetrics` in JSON format. Add header `Authorization` with your `<unique secret key>`. Please use daily aggregation of metrics during export. You can set automation to export data periodically.
---
+++ app.py
@@ -0,0 +1,125 @@
+from fastapi import FastAPI, Request, HTTPException, status
+import uvicorn
+import os
+import asyncio
+from os import path
+import logging
+import frontmatter
+
+# shared secret key for authentication
+SECRET_KEY = os.getenv("AH2O_SECRET_KEY")
+if not SECRET_KEY:
+    raise Exception("Secret key not configured")
+
+# location of Obsidian daily notes
+DAILY_NOTES_LOCATION = os.getenv("AH2O_DAILY_NOTES_LOCATION")
+if not DAILY_NOTES_LOCATION:
+    raise Exception("Daily notes location not configured")
+
+# Convert lb to kg by default
+CONVERT_LB_TO_KG = os.getenv("AH2O_CONVERT_LB_TO_KG", "true").lower() == "true"
+
+app = FastAPI()
+
+
+@app.get("/health")
+async def health():
+    return {"message": "Healthy!"}
+
+
+@app.post("/healthmetrics")
+async def save_json_to_file(request: Request):
+    auth_header = request.headers.get("Authorization")
+    if auth_header != SECRET_KEY:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Unauthorized"
+        )
+
+    json_data = await request.json()
+    asyncio.create_task(parse_metrics(json_data))
+    return {"message": "Success"}
+
+
+def lb_to_kg(lb):
+    return lb * 0.453592
+
+
+def date_to_path(date: str):
+    filename = date.split(" ")[0] + ".md"
+    return path.join(DAILY_NOTES_LOCATION, filename)
+
+
+def save_to_frontmatter(note_path: str, key: str, value: str, units: str):
+    if not path.exists(note_path):
+        open(note_path, "w").close()
+
+    post = frontmatter.load(note_path)
+    post[key] = f"{value} {units}"
+    with open(note_path, "w") as f:
+        f.write(frontmatter.dumps(post))
+
+
+def parse_and_save(metric: dict):
+    # example:
+    # {
+    #     "name": "basal_energy_burned",
+    #     "units": "kcal",
+    #     "data": [
+    #         {
+    #             "source": "",
+    #             "date": "2024-09-20 00:00:00 -0700",
+    #             "qty": 1420.710739391346
+    #         },
+    #         {
+    #             "date": "2024-09-21 00:00:00 -0700",
+    #             "source": "",
+    #             "qty": 1428.6691966059852
+    #         },
+    #         {
+    #             "qty": 749.7120640026696,
+    #             "date": "2024-09-22 00:00:00 -0700",
+    #             "source": ""
+    #         }
+    #     ]
+    # },
+
+    if ("name" not in metric) or ("units" not in metric) or ("data" not in metric):
+        logging.error(f"Invalid metric: {metric}")
+        return
+
+    name = metric["name"]
+    units = metric["units"]
+    data = metric["data"]
+
+    for value in data:
+        if "date" not in value or "qty" not in value:
+            logging.error(f"Invalid value: {value}")
+            continue
+
+        qty = value["qty"]
+        date = date_to_path(value["date"])
+
+        if units == "lb" and CONVERT_LB_TO_KG:
+            qty = lb_to_kg(qty)
+            units = "kg"
+
+        save_to_frontmatter(date, name, qty, units)
+
+
+async def parse_metrics(data: dict):
+    metrics = data.get("data", {}).get("metrics", [])
+    if not metrics:
+        logging.error("No metrics found in the data")
+        logging.debug(data)
+        return
+
+    for metric in metrics:
+        try:
+            parse_and_save(metric)
+        except Exception as e:
+            logging.error(f"Error parsing metric: {metric}")
+            logging.error(e)
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
